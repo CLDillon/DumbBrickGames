26b281e01bd52b76eb3f100c65c55158b1803d72
cdillon <cassandraleighdillon@gmail.com>
2016-10-21 16:20:05 +0100

    The assets and project settings. First initial commit.

---
+++ AINT354/Assets/scripts/charectermove.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charectermove : MonoBehaviour {
+
+    
+
+    private Transform movePlayer;
+    public float force = 10;
+    public float gravity = 9f;
+    public Camera MainCamera;
+    private Vector3 direction;
+    private Rigidbody charecter;
+
+	// Use this for initialization
+	void Start () {
+
+        movePlayer = transform;
+        charecter = GetComponent<Rigidbody>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.tag == "Player")
+                {
+                    Vector3 screenPos = MainCamera.WorldToScreenPoint(movePlayer.position);
+                   
+                    screenPos.z = 0;
+                    direction = (screenPos - Input.mousePosition).normalized;
+                }
+            }Aim();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            charecter.velocity = direction * force;
+        }
+	}
+
+
+
+    private void Aim()
+    {
+        float Sx = direction.x * force;
+        float Sy = direction.y * force;
+
+    }
+}
