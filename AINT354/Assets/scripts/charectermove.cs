aad44e8fec13bd034c4543761b48698d9cf6c9c0
cdillon <cassandraleighdillon@gmail.com>
2016-10-28 16:05:03 +0100

    My character move is now working. Space man can jump along now. Now i have to work on the camera as i want my play to be able to jump off screen and die :-)

--- AINT354/Assets/scripts/charectermove.cs
+++ AINT354/Assets/scripts/charectermove.cs
@@ -9,7 +9,7 @@ public class charectermove : MonoBehaviour {
     public float force = 10;
     public float gravity = 9f;
     public Camera MainCamera;
-    private Vector3 direction;
+    private Vector2 direction;
     private Rigidbody2D charecter;
 
 	// Use this for initialization
@@ -21,28 +21,45 @@ public class charectermove : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+	void FixedUpdate () {
+        //if (Input.GetMouseButtonDown(0))
+        //   {
+        //       RaycastHit hit;
+        //       Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray, out hit))
-            {
-                if(hit.transform.tag == "Player")
-                {
-                    Vector3 screenPos = MainCamera.WorldToScreenPoint(movePlayer.position);
-                   
-                    screenPos.z = 0;
-                    direction = (screenPos - Input.mousePosition).normalized;
-                }
-            }
+
+        //       if(Physics.Raycast(ray, out hit))
+        //       {
+        //           Debug.DrawRay(transform.position, hit.point, Color.red);
+        //           if (hit.transform.tag == "Player")
+        //           {
+        //               Vector3 screenPos = MainCamera.WorldToScreenPoint(movePlayer.position);
+
+        //               screenPos.z = 0;
+        //               direction = (screenPos - Input.mousePosition).normalized;
+        //           }
+        //       }
+        //   }
+
+        //   if (Input.GetMouseButtonUp(0))
+        //   {
+        //       charecter.velocity = direction * 1000;
+        //   }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 screenPos = MainCamera.WorldToScreenPoint(movePlayer.position);
+
+            screenPos.z = 0;
+            direction = (screenPos - Input.mousePosition).normalized;
         }
 
         if (Input.GetMouseButtonUp(0))
         {
             charecter.velocity = direction * force;
         }
+
+        Debug.Log(charecter.velocity);
 	}
 
 
